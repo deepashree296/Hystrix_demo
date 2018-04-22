711ff08fb053c1ab170586bbc3c754480cd75c08
Abhishek <abhi.hakunamatata@gmail.com>
2018-04-22 16:11:44 +0530

    withvalidation endpoint added

--- bookstore/index.js
+++ bookstore/index.js
@@ -8,6 +8,35 @@ const port = 3030;
 const log = new Log('DEBUG', fs.createWriteStream('app.log', { flags: 'a' }));
 
 let endPoint;
+const userData = [
+  {
+    userId: 1,
+    bookId: [3, 4, 5],
+  },
+  {
+    userId: 2,
+    bookId: [6, 7, 8],
+  },
+];
+
+const getDataForUserId = (userId) => {
+  let userDataObject = null;
+  userData.some((element) => {
+    if (element.userId === userId) {
+      userDataObject = element;
+      return true;
+    }
+    return false;
+  });
+  return userDataObject;
+};
+
+const isBookInUserObject = (userDataObject, bookId) => userDataObject.bookId.some((element) => {
+  if (element === bookId) {
+    return true;
+  }
+  return false;
+});
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -73,6 +102,32 @@ app.post(endPoint, (req, res) => {
     data: 'How did you like my server error!',
   });
 });
+
+// endpoint with data validation
+endPoint = '/recommended-withvalidation';
+app.post(endPoint, (req, res) => {
+  log.info(`POST ${endPoint} - Data - ${JSON.stringify(req.body)}`);
+  if (!req.body.bookId || !req.body.userId) {
+    res.status(400).json({
+      data: 'Please provide valid book id and user id',
+    });
+    return;
+  }
+  const userId = parseInt(req.body.userId, 10);
+  const bookId = parseInt(req.body.bookId, 10);
+  const userDataObject = getDataForUserId(userId);
+  const foundBookId = isBookInUserObject(userDataObject, bookId);
+  if (foundBookId) {
+    res.status(200).json({
+      data: `You asked for details of bookId: ${req.body.bookId}, which we found`,
+    });
+  } else {
+    res.status(400).json({
+      data: `You asked for details of bookId: ${req.body.bookId}, which we could not find`,
+    });
+  }
+});
+
 
 const main = () => {
   app.listen(port);
